c2235b8dcb3624eb26a29fa7458ab245eaa2a793
ZackMisso <zack441@mac.com>
2017-01-21 17:19:16 -0600

    Trying this

---
+++ Dummy/Source/Dummy/DummyGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DummyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUMMY_API ADummyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
